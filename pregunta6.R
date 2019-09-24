d44cc8d265b08b1d84c7f5c957d605f22058268b
ger <gerson.villugas.a@uni.pe>
2019-09-23 21:58:22 -0500

    ga

--- pregunta6.R
+++ pregunta6.R
@@ -1,15 +1,30 @@
 rm(list = ls())
+#x~f(x)   0<x<0.5
+#y~g(x)=Exp(1)  0<x
+#lambda =1 es conveniente
+#c=1/(1-e^(0.05))
+#f(x)/c*g(x)=1
+
 lambda<-1
-k<-100
+k<-1000
 x<-rep(0,k)
+
 c<-(1/(1-exp(-0.05)))
+
 for (i in 1:k) {
   y=-(1/lambda)*log(runif(1,0,1))
-  u2<-runif(1,0,1)
-  while (u2>=(1/c*lambda)*exp(lambda*x-x)*(1/(1-exp(-0.05)))) {
+  #u2<-runif(1,0,1)
+  while (y>0.05){
     y=-(1/lambda)*log(runif(1,0,1))
-    u2<-runif(1,0,1)
   }
   x[i]<-y
 }    
-hist(y)
+hist(x)
+
+n<-1:k
+x_c<-cumsum(x)/n
+plot(n,x_c,main = "E[x]",type = "l")
+x_c[length(n)]
+
+
+
